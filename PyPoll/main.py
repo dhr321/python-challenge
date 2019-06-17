a36badf095c759e3759a67b2d68aa69cb2018331
Dave Rodriguez <dhr321@gmail.com>
2019-06-16 21:31:31 -0500

    Count total number of votes.

---
+++ PyPoll/main.py
@@ -0,0 +1,54 @@
+# Script for PyBank assignment.
+# Dave Rodriguez. david.h.rodriguez@gmail.com
+
+# Import dependencies
+import os
+import csv
+import numpy as np
+
+
+# Initialize variables.
+votecount = 0
+votes = 0
+
+# Lists to store data.
+voters = []
+all_candidates = []
+unique_candidates = []
+
+
+# os.getcwd()
+
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+# Open .csv data file.
+with open(csvpath, newline='') as csvfile:
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    # print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header.
+    # Populate a list of all the selected candidate names
+    for row in csvreader:
+        all_candidates.append(row[2])
+        votecount = votecount + 1
+
+    # Find unique candidate names and add them to a new list.
+    # Found syntax example here: https://stackoverflow.com/questions/12897374/get-unique-values-from-a-list-in-python/37163210
+    for x in all_candidates:
+        if x not in unique_candidates:
+            unique_candidates.append(x)
+
+    print(unique_candidates)
+
+        #pl.append(row[1])
+        #monthcount = monthcount + 1
+
+    print("--------------------------")
+    print("Election Results")
+    print("--------------------------")
+    print("Total Votes: " + str(votecount))
+    print("--------------------------")
